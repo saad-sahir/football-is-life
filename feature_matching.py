03be80697d7256f24c572a8bba0dd73a88ab9a96
saad-sahir <133065206+saad-sahir@users.noreply.github.com>
2023-12-05 13:42:26 +0100

    feature matching
    honestly not very useful rn but it'll become useful

--- feature_matching.py
+++ feature_matching.py
@@ -1,19 +1,15 @@
-from line_model import lsd, preprocess_im
 import cv2 as cv
 import numpy as np
 import pandas as pd
+from line_model import lsd, preprocess_im
+from sklearn.neighbors import NearestNeighbors
+
 
 def show_image(canvas):
     cv.imshow('image', canvas)
     cv.waitKey(0)
     cv.destroyAllWindows
 
-def draw_lines(lines, canvas):
-    for line in lines:
-        y1, x1, y2, x2 = line
-        cv.line(canvas, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 255), 1)
-
-    show_image(canvas)
 
 def radar_features(radar, save=False):
     pitch = lsd(radar)
@@ -29,30 +25,30 @@ def image_features(im, save=False):
         df.to_csv('features_test.csv')
     return image_features
 
-image = cv.imread('images/test/test.jpg')
-pitch = cv.imread('images/pitch.png')
 
-pitch_features = np.array(pd.read_csv('features/features_pitch.csv').values[:, 1:])
-field_features = np.array(pd.read_csv('features/features_test.csv').values[:, 1:])
+def prep_features(pitch_features, field_features):
+    p = np.array(pitch_features)
+    f = np.array(field_features)
 
-h_pitch, w_pitch = pitch.shape[:2]
-h_image, w_image = image.shape[:2]
+    p = np.unique(p, axis=0)
+    f = np.unique(f, axis=0)
 
-normalize_points = lambda points, height, width: [
-    (y1 / height, x1 / width, y2 / height, x2 / width)
-    for y1, x1, y2, x2 in points
-]
+    return p, f
 
-norm_pitch = normalize_points(pitch_features, h_pitch, w_pitch)
-norm_image = normalize_points(image_features, h_image, w_image)
-points_pitch = [(x1, y1) for _, x1, _, y1 in norm_pitch]
-points_test = [(x2, y2) for _, x2, _, y2 in norm_image]
-points_pitch = np.array(points_pitch, dtype=np.float32)
-points_test = np.array(points_test, dtype=np.float32)
+def lines_kp(lines):
+    kp = []
+    for line in lines:
+        kp.append(line[:2])
+        kp.append(line[2:])
+    return np.unique(np.array(kp), axis=0)
 
-H, _ = cv.findHomography(points_pitch, points_test, cv.RANSAC)
+image = cv.imread('data/images/test/test.jpg')
+pitch = cv.imread('data/images/pitch.png')
 
-h, w = image.shape[:2]
-warped = cv.warpPerspective(pitch, H, (w, h))
+pitch_features = pd.read_csv('features/features_pitch.csv', index_col='Unnamed: 0').values
+field_features = pd.read_csv('features/features_test.csv', index_col='Unnamed: 0').values
 
-show_image(warped)
+p, f = prep_features(pitch_features, field_features)
+
+kp_p = lines_kp(p)
+kp_f = lines_kp(f)
